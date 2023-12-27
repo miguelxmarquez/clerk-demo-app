bf26696771ef69f7b31ad1d0a9fca5a95d0e6b82
Miguel Márquez <miguelxmarquez@gmail.com>
2023-12-27 10:24:17 -0500

    Role Based Access Control
    Implement basic Role Based Access Control (RBAC) with metadata

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
+import { checkRole } from '@/utils/roles'
+import { redirect } from 'next/navigation'
 
 export default function DashboardPage() {
+	if (!checkRole('admin')) {
+		redirect('/')
+	}
+
 	return (
 		<div className="p-10 content-center">
 			<h1 className="text-4xl mb-4">Dashboard</h1>
